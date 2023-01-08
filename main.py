8597cb20fe4a4335dcc8ce38527c016f55eca447
Luke Prior <22492406+Uskompuf@users.noreply.github.com>
2023-01-08 20:45:16 +1100

    fix encoding

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ r = requests.get('https://api.cdr.gov.au/cdr-register/v1/banking/data-holders/br
 available_brands = r.json()
 sorted_brands = sorted(available_brands['data'], key=lambda item: item['brandName'])
 
-f = open("raw/complete.txt", "w")
+f = open("raw/complete.txt", "w", encoding="utf8", errors='ignore')
 for brand in sorted_brands:
     f.write(f"{brand['brandName']}: {brand['publicBaseUri']}/cds-au/v1\n")
 f.close()
